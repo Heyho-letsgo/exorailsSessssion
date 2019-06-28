436cbf5d5edb074e4b274cf1c1056c0d414f1c83
Ubuntu <ubuntu@ip-172-31-21-77.us-east-2.compute.internal>
2019-06-28 08:58:29 +0000

    Création des méthodes dans Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+    
+    def create
+    user = User.find_by(email: params[:email])
+        if user && user.authenticate(params:[:password])
+            session[:user_id] = user.id
+            flash[:succes] = 'Succesfull Logged In !'
+            redirect_to '/contacts'
+        else
+            flash[:warning] = 'Invalid User Name or Password !'
+            redirect_to '/login'
+        end
+    end
+    
+    def destroy
+        session[:user_id] = nil
+        flash[:succes] = 'Successfully Logged Out !'
+        redirect_to '/login'
+    end
+
 end
